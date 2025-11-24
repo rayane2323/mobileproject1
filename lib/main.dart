69bf32bf63a413d13d5750332dcd767d8df62206
rayan2323 <rourounasser123@gmail.com>
2025-11-24 13:28:25 +0200

    adding dropdown menu

--- lib/main.dart
+++ lib/main.dart
@@ -26,33 +26,47 @@ class TaskHelperPage extends StatefulWidget {
 
 class _TaskHelperPageState extends State<TaskHelperPage> {
   final TextEditingController _taskController = TextEditingController();
+  final List<String> _categories = ['Work', 'School', 'Personal'];
+  String _selectedCategory = 'Work';
   @override
   void dispose() {
-  _taskController.dispose();
-  super.dispose();
-}
+    _taskController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("To-Do List"),
-        centerTitle: true,
-      ),
-     body: Padding(
-  padding: const EdgeInsets.all(20),
-  child: Column(
-    children: [
-      TextField(
-        controller: _taskController,
-        decoration: const InputDecoration(
-          labelText: 'Enter Task',
-          border: OutlineInputBorder(),
+      appBar: AppBar(title: const Text("To-Do List"), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: _taskController,
+              decoration: const InputDecoration(
+                labelText: 'Enter Task',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            DropdownButton<String>(
+              value: _selectedCategory,
+              onChanged: (String? value) {
+                setState(() {
+                  _selectedCategory = value!;
+                });
+              },
+              items: _categories
+                  .map<DropdownMenuItem<String>>(
+                    (cat) =>
+                        DropdownMenuItem<String>(value: cat, child: Text(cat)),
+                  )
+                  .toList(),
+            ),
+          ],
         ),
       ),
-    ],
-  ),
-),
     );
   }
 }
